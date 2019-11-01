94349c9cee96e80b111789dc129f712addbca0fe
Enrique Georg <enriquegeorg@gmail.com>
2019-10-31 22:40:45 -0300

    criamos a classe page

---
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,10 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hcode Store</title>
+  </head>
+  <body>
+    <h1>hello!</h1>
+  </body>
+</html>
