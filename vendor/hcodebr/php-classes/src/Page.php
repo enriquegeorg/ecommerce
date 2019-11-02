0ae1ed913f3482857dc955091d07ee708970718c
Enrique Georg <enriquegeorg@gmail.com>
2019-11-02 14:47:24 -0300

    adicionado pagina de adminitradores

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -7,13 +7,15 @@ class Page {
   private $tpl;
   private $options = [];
   private $defaults = [
+    "header"=>true,
+    "footer"=>true,
     "data"=>[]
   ];
 
-  public function __construct($opts = array()){
+  public function __construct($opts = array(), $tpl_dir="/views/"){
     $this->options = array_merge($this->defaults, $opts);
     $config = array(
-  					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+  					"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
   					"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
   					"debug"         => false // set to false to improve the speed
   	);
@@ -22,7 +24,7 @@ class Page {
 
     $this->tpl = new Tpl;
     $this->setData($this->options["data"]);
-    $this->tpl->draw("header");
+    if($this->options["header"]==true) $this->tpl->draw("header");
   }
 
   private function setData($data=array()){
@@ -37,7 +39,7 @@ class Page {
   }
 
   public function __destruct(){
-    $this->tpl->draw("footer");
+    if($this->options["footer"]==true) $this->tpl->draw("footer");
   }
 
 }
