1d0320a3e0726eedac9d2150446afcf6a805d0c1
Enrique Georg <enriquegeorg@gmail.com>
2019-11-02 16:58:42 -0300

    loop para mostrar usuários cadastrados

--- index.php
+++ index.php
@@ -39,6 +39,35 @@ $app->get('/admin/logout', function() {
 	header("Location: /admin/login");
 	exit;
 });
+$app->get('/admin/users', function() {
+	User::verifyLogin();
+	$users= User::listAll();
+	// var_dump($users);
+	// exit;
+	$page=new PageAdmin();
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+});
+$app->get('/admin/users/create', function() {
+	User::verifyLogin();
+	$page=new PageAdmin();
+	$page->setTpl("users-create");
+});
+$app->get("/admin/users/:iduser/delete", function($iduser){
+	User::verifyLogin();
+});
+$app->get('/admin/users/:iduser', function($iduser) {
+	User::verifyLogin();
+	$page=new PageAdmin();
+	$page->setTpl("users-update");
+});
+$app->post("/admin/users/create", function(){
+	User::verifyLogin();
+});
+$app->post("/admin/users/:iduser", function($iduser){
+	User::verifyLogin();
+});
 $app->run();
 
  ?>
